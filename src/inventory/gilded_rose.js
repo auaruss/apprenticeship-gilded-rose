123134a6d88fedcb2e18da8fd31b74fb6ad1bb31
Alice Russell <alice@heysparkbox.com>
2021-07-28 14:34:37 -0500

    refactor: refactor gilded_rose.js
    References FSA21V2-51

--- src/inventory/gilded_rose.js
+++ src/inventory/gilded_rose.js
@@ -1,3 +1,5 @@
+import { NORMAL_ITEM_DEPRECATION } from './gilded_rose.config.json';
+
 // Item constructor. DO NOT MODIFY OR THE GOBLIN WILL EAT YOU!
 export function Item(name, sell_in, quality) {
   this.name = name;
@@ -5,58 +7,50 @@ export function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-/*
-* Update inventory
-* @param {Item[]} items - an array of Items representing the inventory to be updated
-* Example usage:
-
-const items = [
-  new Item('+5 Dexterity Vest', 10, 20),
-  new Item('Aged Brie', 2, 0),
-  new Item('Elixir of the Mongoose', 5, 7),
-  new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-  new Item('Conjured Mana Cake', 3, 6),
-];
-
-updateQuality(items);
-*/
-export function updateQuality(items) {
-  for (let i = 0; i < items.length; i++) {
-    switch(items[i].name) {
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        updateBackstagePasses(items[i]);
-        break;
-      case 'Sulfuras, Hand of Ragnaros':
-        break;
-      case 'Aged Brie':
-        updateAgedBrie(items[i]);
-        break;
-      default:
-        updateUnspecifiedItem(items[i]);
-        break;
-    }
-
-    guardItemQualityAndDeprecateSellIn(item);
-  }
+/**
+ * Update inventory
+ * @param {Item[]} items - an array of Items representing the inventory to be updated
+ * @returns nothing
+ */
+export const updateQuality = (items) => {
+  items.forEach(item => {
+    updateItem(item);
+    guardItemQuality(item);
+    deprecateSellIn(item);
+  });
 }
 
 
-function updateBackstagePasses(item) {
+const updateItem = (item) => {
+  switch(item.name) {
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      updateBackstagePasses(item);
+      break;
+    case 'Sulfuras, Hand of Ragnaros':
+      break;
+    case 'Aged Brie':
+      updateAgedBrie(item);
+      break;
+    default:
+      updateUnspecifiedItem(item);
+      break;
+  }
+}
+
+const updateBackstagePasses = (item) => {
   if      (item.sell_in <= 0) item.quality = 0;
   else if (item.sell_in < 6)  incrementQuality(item, 3);
   else if (item.sell_in < 10) incrementQuality(item, 2);
   else                        incrementQuality(item, 1);
 }
 
-function updateAgedBrie(item) {
+const updateAgedBrie = (item) => {
   (item.sell_in < 0) 
   ? incrementQuality(item, 2)
   : incrementQuality(item, 1);
 }
 
-function updateUnspecifiedItem(item) {
-  const NORMAL_ITEM_DEPRECATION = 1;
+const updateUnspecifiedItem = (item) => {
 
   const normalOrConjuredModifier =
     (item.name.toLowerCase().includes('conjured')) ? 2 : 1;
@@ -68,15 +62,17 @@ function updateUnspecifiedItem(item) {
   incrementQuality(item, -deprecation);
 }
 
-function incrementQuality(item, incrementBy) {
+const incrementQuality = (item, incrementBy) => {
   if (item.quality >= 50 && incrementBy > 0) return;
   item.quality += incrementBy;
 }
 
-function guardItemQualityAndDeprecateSellIn(item) {
+const guardItemQuality = (item) => {
   if (item.quality < 0)
     item.quality = 0;
+}
 
+const deprecateSellIn = (item) => {
   if (item.name !== 'Sulfuras, Hand of Ragnaros') 
     item.sell_in--;
 }
