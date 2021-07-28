8ad8e99e7b8b55c735d3838ee66dea00cb6f474e
Alice Russell <alice@heysparkbox.com>
2021-07-28 14:34:37 -0500

    refactor: refactor gilded_rose.js
    References FSA21V2-51

--- src/inventory/gilded_rose.js
+++ src/inventory/gilded_rose.js
@@ -1,4 +1,9 @@
-import { NORMAL_ITEM_DEPRECATION } from './gilded_rose.config.json';
+import {
+  NORMAL_ITEM_DEPRECATION,
+  CONJURED_MODIFIER,
+  NORMAL_MODIFIER,
+  MAX_QUALITY_INCREASE
+} from './gilded_rose.config.json';
 
 // Item constructor. DO NOT MODIFY OR THE GOBLIN WILL EAT YOU!
 export function Item(name, sell_in, quality) {
@@ -14,14 +19,14 @@ export function Item(name, sell_in, quality) {
  */
 export const updateQuality = (items) => {
   items.forEach(item => {
-    updateItem(item);
+    updateItemQuality(item);
     guardItemQuality(item);
     deprecateSellIn(item);
   });
 }
 
 
-const updateItem = (item) => {
+const updateItemQuality = (item) => {
   switch(item.name) {
     case 'Backstage passes to a TAFKAL80ETC concert':
       updateBackstagePasses(item);
@@ -39,21 +44,21 @@ const updateItem = (item) => {
 
 const updateBackstagePasses = (item) => {
   if      (item.sell_in <= 0) item.quality = 0;
-  else if (item.sell_in < 6)  incrementQuality(item, 3);
-  else if (item.sell_in < 10) incrementQuality(item, 2);
-  else                        incrementQuality(item, 1);
+  else if (item.sell_in <  6) incrementQuality(item, 3 * NORMAL_MODIFIER);
+  else if (item.sell_in < 10) incrementQuality(item, 2 * NORMAL_MODIFIER);
+  else                        incrementQuality(item, NORMAL_MODIFIER);
 }
 
 const updateAgedBrie = (item) => {
   (item.sell_in < 0) 
-  ? incrementQuality(item, 2)
-  : incrementQuality(item, 1);
+  ? incrementQuality(item, 2 * NORMAL_MODIFIER)
+  : incrementQuality(item, NORMAL_MODIFIER);
 }
 
 const updateUnspecifiedItem = (item) => {
 
   const normalOrConjuredModifier =
-    (item.name.toLowerCase().includes('conjured')) ? 2 : 1;
+    (item.name.toLowerCase().includes('conjured')) ? CONJURED_MODIFIER : NORMAL_MODIFIER;
         
   const deprecation = (item.sell_in >= 0)
     ? normalOrConjuredModifier * NORMAL_ITEM_DEPRECATION
@@ -63,7 +68,7 @@ const updateUnspecifiedItem = (item) => {
 }
 
 const incrementQuality = (item, incrementBy) => {
-  if (item.quality >= 50 && incrementBy > 0) return;
+  if (item.quality >= MAX_QUALITY_INCREASE && incrementBy > 0) return;
   item.quality += incrementBy;
 }
 
